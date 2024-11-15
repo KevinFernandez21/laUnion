345f4d1c7a21724c65ea64f49be0ddb2f84b96f2
KevinFernandez21 <kdfernan@espol.edu.ec>
2024-11-15 11:02:07 -0500

    Delete api

--- backend/test/testgemini.py
+++ backend/test/testgemini.py
@@ -3,8 +3,14 @@ import google.generativeai as genai
 from dotenv import load_dotenv
 import os
 load_dotenv()
-API_KEY = os.getenv('GOOGLE_API_KEY')
-print(API_KEY)
+
+genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
 model = genai.GenerativeModel("gemini-1.5-flash")
-sample_pdf = genai.upload_file(media / "test.pdf")
-sampla_file = PIL.Image.open("./prueba.png")
+sample_file = genai.upload_file(path="jetpack.jpg",
+                            display_name="Jetpack drawing")
+
+# Prompt the model with text and the previously uploaded image.
+response = model.generate_content([sample_file, "Describe how this product might be manufactured."])
+
+print(response.text)
+
